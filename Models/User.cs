0f4e80a938e0042fbc8b389c0f7303bb4d6d6123
Ventura <ventura.py3@gmail.com>
2025-01-31 19:33:30 -0300

    feat: Adds basic model classes

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+namespace BaltaBlogEF.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Bio { get; set; }
+    public string Image { get; set; }
+    public string Slug { get; set; }
+    // public List<Role> Roles { get; set; }
+
+    // public User()
+    //     => Roles = new List<Role>();
+}
---
+++ Models/UserRole.cs
@@ -0,0 +1,7 @@
+namespace BaltaBlogEF.Models;
+
+public class UserRole
+{
+    public int UserId { get; set; }
+    public int RoleId { get; set; }
+}
